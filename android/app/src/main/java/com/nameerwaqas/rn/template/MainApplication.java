9cb531486ddf1fc9f06dcbade6150756e551d73c
NameerWaqas <nameerwaqas321@gmail.com>
2021-12-05 02:21:27 +0500

    🔧 config: update app name and bundle name

--- android/app/src/main/java/com/nameerwaqas/rn/template/MainApplication.java
+++ android/app/src/main/java/com/nameerwaqas/rn/template/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rntypescript;
+package com.nameerwaqas.rn.template;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rntypescript.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.nameerwaqas.rn.template.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
